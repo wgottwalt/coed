8264594df4099eea71e873ee902003eb40b1be39
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2020-02-26 12:59:49 +0100

    updated translations
    Updated all important parts of the Spanish translation file.

--- sources/i18n_Spanish.ts
+++ sources/i18n_Spanish.ts
@@ -16,7 +16,7 @@
     <message>
         <location filename="EditorWindow.ui" line="95"/>
         <source>I18N_SMARTFONT</source>
-        <translation type="unfinished"></translation>
+        <translation>SF</translation>
     </message>
     <message>
         <location filename="EditorWindow.ui" line="105"/>
@@ -26,7 +26,7 @@
     <message>
         <location filename="EditorWindow.ui" line="108"/>
         <source>I18N_WORDWRAP</source>
-        <translation type="unfinished"></translation>
+        <translation>WW</translation>
     </message>
     <message>
         <location filename="EditorWindow.ui" line="118"/>
@@ -36,7 +36,7 @@
     <message>
         <location filename="EditorWindow.ui" line="121"/>
         <source>I18N_LINENUMBERS</source>
-        <translation type="unfinished"></translation>
+        <translation>LN</translation>
     </message>
     <message>
         <location filename="EditorWindow.ui" line="134"/>
@@ -46,7 +46,7 @@
     <message>
         <location filename="EditorWindow.ui" line="137"/>
         <source>I18N_LINEHIGHLIGHT</source>
-        <translation type="unfinished"></translation>
+        <translation>LH</translation>
     </message>
     <message>
         <location filename="EditorWindow.ui" line="150"/>
@@ -56,7 +56,7 @@
     <message>
         <location filename="EditorWindow.ui" line="153"/>
         <source>I18N_TAB</source>
-        <translation type="unfinished"></translation>
+        <translation>TTS</translation>
     </message>
     <message>
         <location filename="EditorWindow.ui" line="166"/>
@@ -81,7 +81,7 @@
     <message>
         <location filename="EditorWindow.cxx" line="413"/>
         <source>I18N_UNKNOWN_FILE</source>
-        <translation type="unfinished"></translation>
+        <translation>sin_título.txt</translation>
     </message>
     <message>
         <location filename="EditorWindow.cxx" line="454"/>
@@ -119,247 +119,247 @@
     <message>
         <location filename="MainWindow.ui" line="49"/>
         <source>I18N_MM_FILE</source>
-        <translation type="unfinished"></translation>
+        <translation>Archivo</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="61"/>
         <source>I18N_MM_ABOUT</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="70"/>
         <source>I18N_MM_EDIT</source>
-        <translation type="unfinished"></translation>
+        <translation>Editar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="84"/>
         <source>I18N_MM_WINDOW</source>
-        <translation type="unfinished"></translation>
+        <translation>Ventana</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="100"/>
         <source>I18N_MM_CONFIG</source>
-        <translation type="unfinished"></translation>
+        <translation>Preferencias</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="104"/>
         <source>I18N_SM_CONFIG_LANG</source>
-        <translation type="unfinished"></translation>
+        <translation>Idioma</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="116"/>
         <source>I18N_SM_CONFIG_THEME</source>
-        <translation type="unfinished"></translation>
+        <translation>Theme</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="129"/>
         <source>I18N_MM_SEARCH</source>
-        <translation type="unfinished"></translation>
+        <translation>Buscar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="149"/>
         <source>I18N_ME_FILE_EXIT</source>
-        <translation type="unfinished"></translation>
+        <translation>Salir</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="154"/>
         <source>I18N_ME_ABOUT_QT</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de Qt...</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="159"/>
         <source>I18N_ME_ABOUT_APP</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de CoEd...</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="164"/>
         <source>I18N_ME_ABOUT_OXYGEN</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de Oxygen...</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="173"/>
         <source>I18N_ME_FILE_NEW</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuevo</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="178"/>
         <source>I18N_ME_LANG_ENGLISH</source>
-        <translation type="unfinished"></translation>
+        <translation>Inglés</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="183"/>
         <source>I18N_ME_LANG_FRENCH</source>
-        <translation type="unfinished"></translation>
+        <translation>Francés</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="188"/>
         <source>I18N_ME_LANG_GERMAN</source>
-        <translation type="unfinished"></translation>
+        <translation>Alemán</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="193"/>
         <source>I18N_ME_LANG_SPANISH</source>
-        <translation type="unfinished"></translation>
+        <translation>Español</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="198"/>
         <source>I18N_ME_THEME_FUSION</source>
-        <translation type="unfinished"></translation>
+        <translation>Fusion (por defecto)</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="207"/>
         <source>I18N_ME_FILE_OPEN</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir...</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="216"/>
         <source>I18N_ME_FILE_SAVE</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="225"/>
         <source>I18N_ME_FILE_SAVEAS</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar como...</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="234"/>
         <source>I18N_ME_WINDOW_NEXT</source>
-        <translation type="unfinished"></translation>
+        <translation>Siguiente</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="243"/>
         <source>I18N_ME_WINDOW_PREV</source>
-        <translation type="unfinished"></translation>
+        <translation>Anterior</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="248"/>
         <source>I18N_ME_WINDOW_TILE</source>
-        <translation type="unfinished"></translation>
+        <translation>Azulejos</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="253"/>
         <source>I18N_ME_WINDOW_CASCADE</source>
-        <translation type="unfinished"></translation>
+        <translation>Cascada</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="262"/>
         <source>I18N_ME_WINDOW_CLOSE</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerrar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="271"/>
         <source>I18N_ME_WINDOW_CLOSEALL</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerrar todos</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="280"/>
         <source>I18N_ME_EDIT_UNDO</source>
-        <translation type="unfinished"></translation>
+        <translation>Deshacer</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="289"/>
         <source>I18N_ME_EDIT_REDO</source>
-        <translation type="unfinished"></translation>
+        <translation>Rehacer</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="298"/>
         <source>I18N_ME_EDIT_CUT</source>
-        <translation type="unfinished"></translation>
+        <translation>Cortar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="307"/>
         <source>I18N_ME_EDIT_COPY</source>
-        <translation type="unfinished"></translation>
+        <translation>Copiar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="316"/>
         <source>I18N_ME_EDIT_PASTE</source>
-        <translation type="unfinished"></translation>
+        <translation>Pegar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="325"/>
         <source>I18N_ME_EDIT_CLEAR</source>
-        <translation type="unfinished"></translation>
+        <translation>Eliminar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="334"/>
         <source>I18N_ME_EDIT_SELECTALL</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccionar todo</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="339"/>
         <source>I18N_ME_THEME_WINDOWS</source>
-        <translation type="unfinished"></translation>
+        <translation>Windows</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="344"/>
         <source>I18N_ME_THEME_QTCURVE</source>
-        <translation type="unfinished"></translation>
+        <translation>QtCurve</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="352"/>
         <source>I18N_ME_THEME_DARKMODE</source>
-        <translation type="unfinished"></translation>
+        <translation>Dark Mode</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="357"/>
         <source>I18N_ME_WINDOW_MAXIMIZE</source>
-        <translation type="unfinished"></translation>
+        <translation>Maximizar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="362"/>
         <source>I18N_ME_WINDOW_MINIMIZE</source>
-        <translation type="unfinished"></translation>
+        <translation>Minimizar</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="367"/>
         <source>I18N_ME_LANG_DUTCH</source>
-        <translation type="unfinished"></translation>
+        <translation>Holandés</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="372"/>
         <source>I18N_ME_LANG_ITALIAN</source>
-        <translation type="unfinished"></translation>
+        <translation>Italiano</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="377"/>
         <source>I18N_ME_SEARCH_FIND</source>
-        <translation type="unfinished"></translation>
+        <translation>Buscar...</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="382"/>
         <source>I18N_ME_SEARCH_NEXT</source>
-        <translation type="unfinished"></translation>
+        <translation>Buscar siguiente</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="387"/>
         <source>I18N_ME_SEARCH_PREV</source>
-        <translation type="unfinished"></translation>
+        <translation>Buscar anterior</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="392"/>
         <source>I18N_ME_ABOUT_FIRA</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de Fira...</translation>
     </message>
     <message>
         <location filename="MainWindow.cxx" line="118"/>
         <source>I18N_FILEOPEN</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir archivo...</translation>
     </message>
     <message>
         <location filename="MainWindow.cxx" line="119"/>
         <source>I18N_FILEOPEN_FILTER</source>
-        <translation type="unfinished">C/C++ Code (*.c *.cc *.cpp *.cxx *.h *.hpp *.hxx);; Text (*.txt *.asc *.ascii *.md *.doc);; All (*)</translation>
+        <translation>Archivo de fuente C/C++ (*.c *.cc *.cpp *.cxx *.h *.hpp *.hxx);; Archivo de texto (*.txt *.asc *.ascii *.md *.doc);; Todos los archivos (*)</translation>
     </message>
     <message>
         <location filename="MainWindow.cxx" line="140"/>
         <source>I18N_FILESAVE</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar archivo...</translation>
     </message>
     <message>
         <location filename="MainWindow.cxx" line="141"/>
         <source>I18N_FILESAVE_FILTER</source>
-        <translation type="unfinished">C/C++ Code (*.c *.cc *.cpp *.cxx *.h *.hpp *.hxx);; Text (*.txt *.asc *.ascii *.md *.doc);; All (*)</translation>
+        <translation>Archivo de fuente C/C++ (*.c *.cc *.cpp *.cxx *.h *.hpp *.hxx);; Archivo de texto (*.txt *.asc *.ascii *.md *.doc);; Todos los archivos (*)</translation>
     </message>
 </context>
 <context>
@@ -367,42 +367,42 @@
     <message>
         <location filename="SearchWindow.ui" line="12"/>
         <source>I18N_SEARCHWINDOW</source>
-        <translation type="unfinished"></translation>
+        <translation>Buscar</translation>
     </message>
     <message>
         <location filename="SearchWindow.ui" line="51"/>
         <source>I18N_SEARCH_FOR</source>
-        <translation type="unfinished"></translation>
+        <translation>Buscar:</translation>
     </message>
     <message>
         <location filename="SearchWindow.ui" line="64"/>
         <source>I18N_SEARCH_OPTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>Opciones</translation>
     </message>
     <message>
         <location filename="SearchWindow.ui" line="85"/>
         <source>I18N_SEARCH_CASE_SENSITIVE</source>
-        <translation type="unfinished"></translation>
+        <translation>Distinguir mayús/minús</translation>
     </message>
     <message>
         <location filename="SearchWindow.ui" line="92"/>
         <source>I18N_SEARCH_WHOLE_WORDS</source>
-        <translation type="unfinished"></translation>
+        <translation>Sólo palabras enteras</translation>
     </message>
     <message>
         <location filename="SearchWindow.ui" line="118"/>
         <source>I18N_SEARCH_CLOSE</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerrar</translation>
     </message>
     <message>
         <location filename="SearchWindow.ui" line="125"/>
         <source>I18N_SEARCH_PREV</source>
-        <translation type="unfinished"></translation>
+        <translation>Anterior</translation>
     </message>
     <message>
         <location filename="SearchWindow.ui" line="132"/>
         <source>I18N_SEARCH_NEXT</source>
-        <translation type="unfinished"></translation>
+        <translation>Siguiente</translation>
     </message>
 </context>
 </TS>
