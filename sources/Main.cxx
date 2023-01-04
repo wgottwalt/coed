271f9f37fd10f4504c58cf135873cbfed4991928
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2023-01-04 13:21:05 +0100

    cleanup main sourcefile
    Switched to exception catching and dynamic allocation plus proper
    cleanup afterwards.

--- sources/Main.cxx
+++ sources/Main.cxx
@@ -1,19 +1,36 @@
+#include <iostream>
 #include <QApplication>
 #include <QFontDatabase>
+
 #include "MainWindow.hxx"
 
-qint32 main(qint32 argc, char **argv) noexcept(false)
+qint32 main(qint32 argc, char **argv)
 {
-    QApplication app(argc, argv);
-    MainWindow win;
+    QApplication *app = nullptr;
+    MainWindow *win = nullptr;
+    qint32 result = 0;
 
-    QApplication::setApplicationName("CoEd");
-    QApplication::setOrganizationName("LinuxAddicted");
-    QApplication::setApplicationVersion(QString("0.1.0.0 (") + __DATE__ + " " + __TIME__ + ")");
-    QFontDatabase::addApplicationFont(":/Font/SiraCodeRegular");
-    QFontDatabase::addApplicationFont(":/Font/SiraMonoRegular");
+    try
+    {
+        app = new QApplication{argc, argv};
+        win = new MainWindow;
 
-    win.show();
+        QApplication::setApplicationName("CoEd");
+        QApplication::setOrganizationName("LinuxAddicted");
+        QApplication::setApplicationVersion(QString("0.1.0.0 (") + __DATE__ + " " + __TIME__ + ")");
+        QFontDatabase::addApplicationFont(":/Font/SiraCodeRegular");
+        QFontDatabase::addApplicationFont(":/Font/SiraMonoRegular");
 
-    return app.exec();
+        win->show();
+        result = app->exec();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    delete win;
+    delete app;
+
+    return result;
 }
